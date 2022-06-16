b5a9fd91425f8d3bdd6cb72fdee803beb7dc4167
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-06-16 17:51:25 +0900

    mailHogを利用してtest

---
+++ resources/views/email/new_user_introduction.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+
+# 新しいユーザーが追加されました！
+
+{{ $toUser->name }}さん、こんにちは！
+
+@component('mail::panel')
+  新しく{{ $newUser->name }}さんが参加しました！
+@endcomponent
+
+@component('mail::button',['url' => route('tweet.index')])
+  つぶやきを見に行く
+@endcomponent
+@endcomponent
+
+
+
