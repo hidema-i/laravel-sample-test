4e32cf1e7f5b00db1139c7dd591199a1696eed30
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-06-18 23:56:37 +0900

    laravel-Breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,5 +44,9 @@ class User extends Authenticatable
     public function tweets()
     {
         return $this->hasMany(Tweet::class);
+    }
+
+    public function test()
+    {
     }
 }
